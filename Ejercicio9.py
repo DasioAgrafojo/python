48b72bd0212b8cc2980012107c9ed57679a10e42
Dasio <agrafojodasio@gmail.com>
2019-10-18 13:32:02 +0200

    Ask for Password

--- Ejercicio9.py
+++ Ejercicio9.py
@@ -1,3 +1,12 @@
 import sys
-clave="Pepe"
-sys.exit()
+clave="pepe"
+salida="salida"
+entrada=(input("Ingresa un nombre: "))
+while entrada!=salida and entrada!=clave:
+    entrada=(input("Ingresa un nombre: "))
+if entrada=="pepe":
+    print("Has acertado")
+elif entrada=="salir":
+    sys.exit()
+
+    
