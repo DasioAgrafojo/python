0d33660dd3e6e4838c0a6ae4d2ce7f4e643a39d0
Dasio <agrafojodasio@gmail.com>
2019-11-06 14:21:29 +0100

    Simple geometric figure volume calculator

--- Ejercicio12.py
+++ Ejercicio12.py
@@ -1,11 +1,22 @@
-import sys
-option=0
-while option!=1 and option!=2 and option!=3:
-option=input('Qué objeto quieres calcular? \n1-> Cubo\n2-> Esfera\n3-> Cilindro')
+option = "0"
 
-if option==1:
+while (option!="1" and option!="2" and option!="3" ):
+    option=input('Qué objeto quieres calcular? \n1-> Cubo\n2-> Esfera\n3-> Cilindro\n')
+
+if option=="1":
     print('Cubo seleccionado')
-elif option==2:
+    AristaCubo= int (input('Dame la medida de las aristas: '))
+    VolCubo= ( AristaCubo ** 3)
+    print ('Su volumen es ', VolCubo)
+
+elif option=="2":
     print('Esfera seleccionada')
+    RadioEsfera= int (input('Dame la medida del radio: '))
+    VolEsfera= (4.2*(RadioEsfera**3))
+    print ('Su volumen es ', VolEsfera)
 else :
     print('Cilindro seleccionado')
+    RadioCilindro= int (input('Dame el radio del cilindro: '))
+    AlturaCilindro= int (input('Dame la altura del cilindro: '))
+    VolCilindro= (AlturaCilindro*RadioCilindro*6.2832)
+    print ('Su volumen es ', VolCilindro)
